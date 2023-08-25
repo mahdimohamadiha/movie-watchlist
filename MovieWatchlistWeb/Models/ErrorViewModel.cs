1da6690d623e2dddf291e64f14b2cf8fa44838eb
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-08-25 17:05:34 +0330

    Add project files.

---
+++ MovieWatchlistWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieWatchlistWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
