120bc37ac68c7f6e562a5aa3bef0ec05f049bdba
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-08-26 12:13:34 +0330

    Configuring the database and adding the Movies table and installing the required packages

---
+++ MovieWatchlistWeb/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieWatchlistWeb.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
