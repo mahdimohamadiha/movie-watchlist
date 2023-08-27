f4f4ebfac8a58726d8d141d8869d338503f9ea23
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-08-27 05:14:52 +0330

    Create Movie View

---
+++ MovieWatchlistWeb/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieWatchlistWeb.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
