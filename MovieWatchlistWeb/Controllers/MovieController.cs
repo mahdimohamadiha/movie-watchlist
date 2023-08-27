0c474051aa52af9da326651ceee46e107bd750c2
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-08-27 05:29:52 +0330

    Database injection into the MovieController class

--- MovieWatchlistWeb/Controllers/MovieController.cs
+++ MovieWatchlistWeb/Controllers/MovieController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieWatchlistWeb.Data;
 
 namespace MovieWatchlistWeb.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public MovieController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objMovieList = _db.Movies.ToList();
             return View();
         }
     }
